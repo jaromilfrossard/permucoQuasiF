a10a8a612468d1c8b5cd2c3dea124925887ba073
jaromilfrossard <jaromil.frossard@hotmail.com>
2018-05-04 13:14:05 +0200

    add class Matrix

--- R/cluster_quasif_terBraak_logp.R
+++ R/cluster_quasif_terBraak_logp.R
@@ -1,4 +1,4 @@
-#' @importClassesFrom Matrix dgeMatrix dgCMatrix
+#' @importClassesFrom Matrix Matrix
 #' @importMethodsFrom Matrix colMeans colSums
 cluster_quasif_terBraak_logp = function(args){
   link = args$link
